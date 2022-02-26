f582b9dec5d69929982e9592031e4c45ca81ea15
odettelittle <Odettelittle1@gmail.com>
2022-02-26 01:26:32 +0000

    first commit done

---
+++ app.js
@@ -0,0 +1,17 @@
+var pronoun = ['the','our'];
+  var adj = ['fun', 'adorable' ];
+  var noun = ['dog','kind'];
+
+for (let pindex=0; pindex<2; pindex++) {
+  for (let aindex=0; aindex<2; aindex++) {
+    for (let nindex=0; nindex<2; nindex++) {
+      console.log (pronoun [pindex]+adj[aindex]+noun[nindex] + '.com')
+      
+    }
+  }
+}
+
+
+
+
+
